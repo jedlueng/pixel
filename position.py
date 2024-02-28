0ea5361f66be90081c77306e3c3fe337468e7ac6
Jedsada Luengaramsuk <jedsada.io@outlook.com>
2024-02-28 18:38:59 +0700

    4 bots

---
+++ position.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+# Get the current mouse position
+x, y = pyautogui.position()
+
+print(f"The current mouse position is {x}, {y}")
---
+++ pot-2.py
@@ -0,0 +1,232 @@
+import pyautogui
+import time
+
+
+x_seed, y_seed = 856, 1006
+x_water, y_water = 771, 1001
+x_harvest, y_harvest = 924, 997
+
+#Base of w 1 + d 1 
+x1, y1 = 904, 644
+x2, y2 = 973, 643
+x3, y3 = 1038, 641
+x4, y4 = 1109, 645
+x5, y5 = 1164, 644
+x11, y11 = 905, 580
+x12 ,y12 = 971, 574
+x13, y13 = 1039, 580 
+x14, y14 = 1100, 580
+x15 , y15 = 1163, 580
+x21, y21 = 904, 514
+x22, y22 = 966, 514
+x23, y23 = 1045, 514
+x24, y24 = 1100, 516
+x25, y25 = 1171, 516
+x31, y31 = 902, 458 
+x32, y32 = 975, 455
+x33, y33 = 1039, 457
+x34 ,y34 = 1104, 450
+x35, y35 = 1169, 450
+
+
+while True:
+    #Seeding
+    time.sleep(4)
+    pyautogui.click(x_seed,y_seed)
+    #pyautogui.click(x_water,y_water)
+    pyautogui.click(x1, y1) 
+    time.sleep(0.5)
+    pyautogui.click(x2, y2)
+    time.sleep(0.5)
+    pyautogui.click(x3, y3)
+    time.sleep(0.5)
+    pyautogui.click(x4, y4)
+    time.sleep(0.5)
+    pyautogui.click(x5, y5)
+    time.sleep(0.5)
+    pyautogui.click(x11, x11)
+    time.sleep(0.5)
+    pyautogui.click(x12, y12)
+    time.sleep(0.5)
+    pyautogui.click(x13, y13)
+    time.sleep(0.5)
+    pyautogui.click(x14, y14)
+    time.sleep(0.5)
+    pyautogui.click(x15, y15)
+    time.sleep(0.5)
+    pyautogui.click(x21, y22)
+    time.sleep(0.5)
+    pyautogui.click(x23, y23)
+    time.sleep(0.5)
+    pyautogui.click(x24, y24)
+    time.sleep(0.5)
+    pyautogui.click(x25, y25)
+    time.sleep(0.5)
+    pyautogui.click(x31, y32)
+    time.sleep(0.5)
+    pyautogui.click(x33, y33)
+    time.sleep(0.5)
+    pyautogui.click(x34, y34)
+    time.sleep(0.5)
+    pyautogui.click(x35, y35)
+    pyautogui.click(x_seed, y_seed)
+    time.sleep(3)
+    pyautogui.click(x_water,y_water)
+    time.sleep(1)
+    pyautogui.click(x1, y1) 
+    time.sleep(0.5)
+    pyautogui.click(x2, y2)
+    time.sleep(0.5)
+    pyautogui.click(x3, y3)
+    time.sleep(0.5)
+    pyautogui.click(x4, y4)
+    time.sleep(0.5)
+    pyautogui.click(x5, y5)
+    time.sleep(0.5)
+    pyautogui.click(x11, x11)
+    time.sleep(0.5)
+    pyautogui.click(x12, y12)
+    time.sleep(0.5)
+    pyautogui.click(x13, y13)
+    time.sleep(0.5)
+    pyautogui.click(x14, y14)
+    time.sleep(0.5)
+    pyautogui.click(x15, y15)
+    time.sleep(0.5)
+    pyautogui.click(x21, y22)
+    time.sleep(0.5)
+    pyautogui.click(x23, y23)
+    time.sleep(0.5)
+    pyautogui.click(x24, y24)
+    time.sleep(0.5)
+    pyautogui.click(x25, y25)
+    time.sleep(0.5)
+    pyautogui.click(x31, y32)
+    time.sleep(0.5)
+    pyautogui.click(x33, y33)
+    time.sleep(0.5)
+    pyautogui.click(x34, y34)
+    time.sleep(0.5)
+    pyautogui.click(x35, y35)
+    pyautogui.click(x_water,y_water)
+    time.sleep(124)
+    pyautogui.click(x_harvest,y_harvest)
+    time.sleep(1)
+    pyautogui.click(x1, y1) 
+    time.sleep(0.5)
+    pyautogui.click(x2, y2)
+    time.sleep(0.5)
+    pyautogui.click(x3, y3)
+    time.sleep(0.5)
+    pyautogui.click(x4, y4)
+    time.sleep(0.5)
+    pyautogui.click(x5, y5)
+    time.sleep(0.5)
+    pyautogui.click(x11, x11)
+    time.sleep(0.5)
+    pyautogui.click(x12, y12)
+    time.sleep(0.5)
+    pyautogui.click(x13, y13)
+    time.sleep(0.5)
+    pyautogui.click(x14, y14)
+    time.sleep(0.5)
+    pyautogui.click(x15, y15)
+    time.sleep(0.5)
+    pyautogui.click(x21, y22)
+    time.sleep(0.5)
+    pyautogui.click(x23, y23)
+    time.sleep(0.5)
+    pyautogui.click(x24, y24)
+    time.sleep(0.5)
+    pyautogui.click(x25, y25)
+    time.sleep(0.5)
+    pyautogui.click(x31, y32)
+    time.sleep(0.5)
+    pyautogui.click(x33, y33)
+    time.sleep(0.5)
+    pyautogui.click(x34, y34)
+    time.sleep(0.5)
+    pyautogui.click(x35, y35)
+    pyautogui.click(x_harvest,y_harvest)
+
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+    # pyautogui.click(x1, y1)
+    # pyautogui.click(x2, y2)
+
+
+
+# x6, y6 = 
+# x4 , y4 = 897, 651
+# x5, y5 = 
+# x6, y6 = 
+# x7, y7 = 
+# x8, y8 = 
+# x9, y9 = 
+# x10, y10 = 
+# x11, y11 = 
+# x12 ,y12 = 
+# x13, y13 = 
+# x14, y14 = 
+# x15 , y15 = 
+# x16, y16 = 
+# x17, y17 = 
+# x18 , y18 = 
+# x19 , y19 = 
+# x20, y20 = 
+# x21, y21 = 
+# x22, y22 = 
+# x23, y23 = 
+# x24, y24 = 
+# x25, y25 = 
+# x26 , y26 = 
+# x27, y27 = 
+# x28, y28 = 
+# x29, y29 = 
+# x30, y30 = 
+# x31, y31 = 
+# x32, y32 = 
+# x33, y33 = 
+# x34 ,y34 = 
+# x35, y35 = 
+# x36, y36 = 
+# x37 , y37 = 
+# x38, y38 = 
+# x39, y39 = 
+# x40 , y40 = 
+# x41 , y41 = 
+# x42, y42 = 
+# x43, y43 = 
+# x44 ,y44 = 
+# x45, y45 = 
+# x46, y46 = 
+# x47 , y47 = 
+# x48, y48 = 
+# x49, y49 = 
+# x50 , y50 = 
+# x51 , y51 = 
+# x52, y52 = 
+# x53, y53 = 
+# x54 ,y54 = 
+# x55, y55 = 
+# x56, y56 = 
+# x57 , y57 = 
+# x58, y58 = 
+# x59, y59 = 
+# x60 , y60 = 
+
+# while True:
+#     time.sleep(4)
+#     pyautogui.click(x_seed,y_seed)
+#     #pyautogui.click(x_water,y_water)
+#     pyautogui.click(x_4, y_4)
