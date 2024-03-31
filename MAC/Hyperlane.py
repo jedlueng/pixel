081d4735dadeab7a28895daf7ae77be145479270
Jedsada Luengaramsuk <68596277+jedsada-io@users.noreply.github.com>
2024-03-31 20:53:30 +0700

    Summary

---
+++ MAC/Hyperlane.py
@@ -0,0 +1,57 @@
+#JEDLUENG 27/02/2024
+#Potberry farm version 1 
+#Go to farm 4514 from Terraview 
+#Then move and farm 
+#Now this is ok but I will improve later for more efficiency 
+#Still better than doing it by myself
+import pyautogui
+import time
+
+
+
+# Enable failsafe - moving the mouse to the upper-left corner will abort the script
+pyautogui.FAILSAFE = True
+ 
+#From Ce 
+
+while True:
+    time.sleep(5)
+    pyautogui.click(760, 476) 
+    # pyautogui.typewrite('10')
+    pyautogui.click(927, 561)
+    pyautogui.click(722, 616)
+    time.sleep(2)
+    pyautogui.click(745, 757)
+    time.sleep(1)
+    pyautogui.click(761, 748)
+    time.sleep(10)
+    pyautogui.click(739, 872)
+    time.sleep(30)
+    pyautogui.click(1143, 503)
+    #Finished loop ces
+    time.sleep(12)
+    pyautogui.click(717, 383)
+    time.sleep(30)
+    pyautogui.click(760, 476) 
+    # pyautogui.typewrite('10')
+    pyautogui.click(927, 561)
+    pyautogui.click(722, 616)
+    time.sleep(12)
+    pyautogui.click(770, 735)
+    time.sleep(20)
+    pyautogui.click(761, 748)
+    time.sleep(20)
+    pyautogui.click(739, 872)
+    time.sleep(20)
+    pyautogui.click(1143, 503)
+    time.sleep(20)
+    pyautogui.click(722, 386)
+    time.sleep(20)
+
+
+    
+   
+
+
+
+    
---
+++ MAC/Jumper.py
@@ -0,0 +1,82 @@
+#JEDLUENG 27/02/2024
+#Potberry farm version 1 
+#Go to farm 4514 from Terraview 
+#Then move and farm 
+#Now this is ok but I will improve later for more efficiency 
+#Still better than doing it by myself
+import pyautogui
+import time
+
+
+
+# Enable failsafe - moving the mouse to the upper-left corner will abort the script
+pyautogui.FAILSAFE = True
+ 
+#From Ce 
+
+while True:
+    time.sleep(10)
+    #Click amount 
+    pyautogui.click(613, 464)
+    #Type Amount 
+    pyautogui.typewrite('2')
+    #Exchange
+    time.sleep(60)
+    pyautogui.click(459, 555)
+    time.sleep(30)
+    #Start Bridgi0.1ng 
+    pyautogui.click(675, 542)
+    time.sleep(30)
+    pyautogui.click(659, 860)
+    time.sleep(30)
+    pyautogui.click(659, 860)
+    time.sleep(360)
+    pyautogui.click(715, 586)
+    time.sleep(5)
+    pyautogui.click(721, 341)
+
+
+    #Sign 
+
+
+    #Wait 
+
+    # pyautogui.click(760, 476) 
+    # # pyautogui.typewrite('10')
+    # pyautogui.click(927, 561)
+    # pyautogui.click(722, 616)
+    # time.sleep(2)
+    # pyautogui.click(745, 757)
+    # time.sleep(1)
+    # pyautogui.click(761, 748)
+    # time.sleep(10)
+    # pyautogui.click(739, 872)
+    # time.sleep(30)
+    # pyautogui.click(1143, 503)
+    # #Finished loop ces
+    # time.sleep(12)
+    # pyautogui.click(717, 383)
+    # time.sleep(30)
+    # pyautogui.click(760, 476) 
+    # # pyautogui.typewrite('10')
+    # pyautogui.click(927, 561)
+    # pyautogui.click(722, 616)
+    # time.sleep(12)
+    # pyautogui.click(770, 735)
+    # time.sleep(20)
+    # pyautogui.click(761, 748)
+    # time.sleep(20)
+    # pyautogui.click(739, 872)
+    # time.sleep(20)
+    # pyautogui.click(1143, 503)
+    # time.sleep(20)
+    # pyautogui.click(722, 386)
+    # time.sleep(20)
+
+
+    
+   
+
+
+
+    
